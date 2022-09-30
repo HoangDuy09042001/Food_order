40b3cd6516328f75772271638032d254d4c24994
HoangDuy09042001 <duy.ht194748@sis.hust.edu.vn>
2022-09-30 16:59:05 +0700

    update

---
+++ admin/partials/footer.php
@@ -0,0 +1,7 @@
+<div class="footer">
+            <div class="wrapper">
+                <p class='text-center'>Lorem ipsum dolor sit amet consectetur. Deveolop by - <a href="#">Vijay Thapa</a></p>
+            </div>
+        </div>
+    </body>
+</html>
