7c1e68ba26cdf37fea4e3277ed257c25768592b9
HoangDuy09042001 <duy.ht194748@sis.hust.edu.vn>
2022-10-05 22:51:32 +0700

    update

---
+++ historyorder.php
@@ -0,0 +1,68 @@
+<?php
+require_once 'database.php'; 
+// $result = mysqli_query($conn, "SELECT 
+// foodname,
+// price,
+// quantity,
+// total,
+// created
+// FROM bill 
+// WHERE orderfood = ( SELECT MAX(orderfood) FROM bill )");
+$result = mysqli_query($conn, " SELECT historyorder.foodname,
+historyorder.price,
+historyorder.quantity,
+historyorder.total,
+historyorder.created,
+images.image 
+FROM
+(SELECT 
+foodname,
+price,
+quantity,
+total,
+created
+FROM bill 
+WHERE orderfood = ( SELECT MAX(orderfood) FROM bill )) AS historyorder
+INNER JOIN images ON historyorder.foodname = images.foodName
+");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/form/historyorder.css">
+</head>
+<body>
+<div class="table-users">
+   <div class="header">Users</div>
+   
+   <table cellspacing="0">
+      <tr>
+         <th>Picture</th>
+         <th>Name</th>
+         <th>Price</th>
+         <th width="230">Quanty</th>
+      </tr>
+      <?php
+                $i=0;
+                $total=0;
+                while ($row = mysqli_fetch_array($result)) {
+                ?>
+                 <tr>
+                    <td><img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['image']); ?>" alt="" /></td>
+                    <td><?php echo $row['foodname'];?></td>
+                    <td><?php echo $row['price'];?></td>
+                    <td><?php echo $row['quantity'];?></td>
+                </tr>
+                <?php
+                $i++;
+                $total = $row['total'];
+            }
+            ?>
+   </table>
+</div>
+</body>
+</html>
