95e4cbd497a84d78ee67990a5b3e8e974bb87220
HoangDuy09042001 <duy.ht194748@sis.hust.edu.vn>
2022-10-13 03:08:00 +0700

    update

--- process.php
+++ process.php
@@ -134,7 +134,27 @@ if (isset($_POST['item']))
   }
   $conn->close();
  }
+
  if(isset($_POST['search'])){
+  function convert_name ($str) {
+    $str = preg_replace("/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/", 'a', $str);
+    $str = preg_replace("/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/", 'e', $str);
+    $str = preg_replace("/(ì|í|ị|ỉ|ĩ)/", 'i', $str);
+    $str = preg_replace("/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/", 'o', $str);
+    $str = preg_replace("/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/", 'u', $str);
+    $str = preg_replace("/(ỳ|ý|ỵ|ỷ|ỹ)/", 'y', $str);
+    $str = preg_replace("/(đ)/", 'd', $str);
+    $str = preg_replace("/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/", 'A', $str);
+    $str = preg_replace("/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/", 'E', $str);
+    $str = preg_replace("/(Ì|Í|Ị|Ỉ|Ĩ)/", 'I', $str);
+    $str = preg_replace("/(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/", 'O', $str);
+    $str = preg_replace("/(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/", 'U', $str);
+    $str = preg_replace("/(Ỳ|Ý|Ỵ|Ỷ|Ỹ)/", 'Y', $str);
+    $str = preg_replace("/(Đ)/", 'D', $str);
+    $str = preg_replace("/(\“|\”|\‘|\’|\,|\!|\&|\;|\@|\#|\%|\~|\`|\=|\_|\'|\]|\[|\}|\{|\)|\(|\+|\^)/", '-', $str);
+    $str = preg_replace("/( )/", '-', $str);
+    return $str;
+  }
   $search = json_decode($_POST['search'], true);
   $result = $db->query("SELECT * FROM images ORDER BY foodPrice ASC"); 
   $i=0;
@@ -144,7 +164,7 @@ if (isset($_POST['item']))
         print_r($name);
       }else{
         if($i<5){
-          if (str_contains(strtolower($name), strtolower($search))) {
+          if (str_contains(strtolower(convert_name($name)), strtolower(convert_name($search)))) {
             echo '<div class="search-content-item">'.$name.'</div>';
             $i++;
           }
@@ -154,7 +174,40 @@ if (isset($_POST['item']))
       }
   }
   $conn->close();
- } 
+ }
+ if(isset($_POST['valueItemSearch'])){
+  $valueItemSearch = json_decode($_POST['valueItemSearch'], true);
+  $resultTypeFood = $db->query("SELECT * FROM images WHERE foodName = '$valueItemSearch'");
+  while ($row = mysqli_fetch_array($resultTypeFood)) {
+    $type = $row['typeFood'];
+    echo '<div class="food-menu-box" style="background-image:url(\'data:image/jpg;charset=utf8;base64,'.base64_encode($row["image"]).'\');">
+  
+    <div class="food-menu-desc" id="food-menu-desc' .$row['id']. ' ">
+        <h4 class="food-name">'. $row['foodName'] . '</h4>
+        <p class="food-price">$'.$row['foodPrice'] . '</p>
+        <p class="food-detail">' . $row['foodDes'] . '</p>
+        <br>
+    </div>
+    </div>';
+  }
+  $result = $db->query("SELECT * FROM images WHERE typeFood = '$type'");
+  $i=0;
+  while ($row = mysqli_fetch_array($result)) {
+    if( $row['foodName']!==$valueItemSearch){
+      echo '<div class="food-menu-box" style="background-image:url(\'data:image/jpg;charset=utf8;base64,'.base64_encode($row["image"]).'\');">
+    
+                  <div class="food-menu-desc" id="food-menu-desc' .$row['id']. ' ">
+                      <h4 class="food-name">'. $row['foodName'] . '</h4>
+                      <p class="food-price">$'.$row['foodPrice'] . '</p>
+                      <p class="food-detail">' . $row['foodDes'] . '</p>
+                      <br>
+                  </div>
+              </div>';
+     }
+     $i++;
+
+    }
+ }
  if(isset($_POST['history'])){
   $maxresult = mysqli_query($conn, "SELECT 
   orderfood
